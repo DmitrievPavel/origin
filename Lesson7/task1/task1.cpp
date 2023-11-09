33041e93b7c38f2c3f7d3bfcce9857c47f70f821
Pavel Dmitriev <pahounny@gmail.com>
2023-11-09 15:02:51 +0300

    lesson7

---
+++ Lesson7/task1/task1.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+
+#define MODE 0
+
+#if MODE == 1 
+int add(int x, int y) {
+	return x + y;
+}
+#endif
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+#ifndef MODE 
+
+#error Constant is undefined
+
+#endif
+
+#if MODE == 0 
+
+	std::cout << "Работаю в режиме тренировки";
+
+#elif MODE == 1 
+
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	int x = 0; 
+	int y = 0; 
+	std::cout << "Введите число 1: "; 
+	std::cin >> x; 
+	std::cout << "Введите число 2: "; 
+	std::cin >> y; 
+	std::cout << "Результат сложения: " << add(x, y); 
+
+#else 
+
+	std::cout << "Неизвестный режим.Завершение работы";
+
+#endif 
+}
