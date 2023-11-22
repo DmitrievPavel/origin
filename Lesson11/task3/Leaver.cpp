7d7e10c8a66f40430d668967fc6eb5d245d59587
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2023-11-22 20:23:23 +0300

    Add files via upload

---
+++ Lesson11/task3/Leaver.cpp
@@ -0,0 +1,6 @@
+#include "Leaver.h"
+
+std::string Leaver::leave(std::string name) {
+	std::string lv = { "До свидания, " };
+	return lv + name + '!';
+}
---
+++ Lesson11/task3/Leaver.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#ifdef LEAVER_DLL_EXPORTS
+#define LEAVER_DLL_API __declspec(dllexport)
+#else
+#define LEAVER_DLL_API __declspec(dllimport)
+#endif
+
+class Leaver {
+public:
+	LEAVER_DLL_API std::string leave(std::string name);
+};
