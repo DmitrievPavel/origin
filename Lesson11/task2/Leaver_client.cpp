00db07dc2a206e73be73de1ebbc0213be041a150
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2023-11-22 20:22:12 +0300

    Add files via upload

---
+++ Lesson11/task2/Leaver_client.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include "Leaver.h"
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+
+	std::string name;
+	Leaver a;
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << a.leave(name);
+}
+
+
