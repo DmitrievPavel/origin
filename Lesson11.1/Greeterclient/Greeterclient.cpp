c9419e6f302faaf123978e5450a8df7ac4d2d2f8
DmitrievPavel <146075675+DmitrievPavel@users.noreply.github.com>
2023-11-26 20:18:50 +0300

    Add files via upload

---
+++ Lesson11.1/Greeterclient/Greeterclient.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include "Greeter.h"
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+    Greeter a;
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+    std::cout << a.greet(name);
+}
+
